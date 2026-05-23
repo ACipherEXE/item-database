686107246209eb21fadc54a18c60a2360d1ba22d
Christopher Negron <sonycar2000@outlook.com>
2026-05-23 16:37:35 -0400

    making superbase be live in the network as well

--- src/superbaseClient.ts
+++ src/superbaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "http://127.0.0.1:54321";
+const supabaseUrl = "http://192.168.1.23:54321";
 const supabaseKey = "sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH"; // paste the anon key from earlier
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
